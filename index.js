c9a0f3b5a18995f1f0dc08832b3a44de78fdf41c
Parasat Zhangbyrbay <63976887+parasat00@users.noreply.github.com>
2024-01-17 01:37:57 +0600

    nav and about section finished

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const sunAndMoon = document.querySelector('.sunAndMoon');
+const sunAndMoon = document.querySelectorAll('.sunAndMoon');
 const css = document.getElementById('theme');
+const nav_toggle = document.querySelector('.nav_toggle');
+const mobile_nav_toggle = document.querySelector('.mobile_nav_toggle');
+const mobile_nav = document.querySelector('.mobile_nav');
+var mobile_nav_shown = false;
 
 const switchMode = (e) => {
  let theme = localStorage.getItem('currentTheme');
@@ -23,6 +27,21 @@ let setYear = () => {
  year.textContent = new Date().getFullYear();
 }
 
+let mobileNavDisplay = () => {
+ if(!mobile_nav_shown) {
+  nav_toggle.classList.add('hide');
+  mobile_nav.classList.remove('hide');
+ }
+ else {
+  nav_toggle.classList.remove('hide');
+  mobile_nav.classList.add('hide');
+ }
+ mobile_nav_shown=!mobile_nav_shown;
+}
+
 setYear();
-sunAndMoon.addEventListener('click', switchMode);
+sunAndMoon.forEach(e => e.addEventListener('click', switchMode)); 
 switchMode();
+
+nav_toggle.addEventListener('click', mobileNavDisplay);
+mobile_nav_toggle.addEventListener('click', mobileNavDisplay);
